b81e8d25ab2c45ae1c1eaa6eb8dec100e6b0c8d1
shivambhor564 <Shivam.Bhor@cognizant.com>
2025-06-11 10:53:48 +0530

    latest changes

--- CartModule/src/main/java/com/cts/Config/BookFeignClient.java
+++ CartModule/src/main/java/com/cts/Config/BookFeignClient.java
@@ -1,4 +1,16 @@
 package com.cts.Config;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(url="http://localhost:9001", value="BOOK")
 public interface BookFeignClient {
-}
+
+    @GetMapping("/bookstore/book/{Id}")
+    public BookDto getBooks(@PathVariable Long Id);
+
+    @PutMapping("/bookstore/book/{Id}")
+    public BookDto updatebook(@PathVariable Long Id, @RequestBody BookDto bookDTO);
