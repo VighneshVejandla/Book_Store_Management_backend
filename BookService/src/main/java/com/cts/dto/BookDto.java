3a0538a4de7044b87a2a095bff9aadbd6c2ba9dd
Vighnesh404 <Vejandla.Vighnesh@cognizant.com>
2025-06-26 10:17:16 +0530

    Changes in Book and Gateway

--- BookService/src/main/java/com/cts/dto/BookDto.java
+++ BookService/src/main/java/com/cts/dto/BookDto.java
@@ -13,57 +13,7 @@ public class BookDto {
 	
 	private AuthorDto author;
 	private CategoryDto category;
-	
-	
-	
-	
-	public Long getBookId() {
-		return bookId;
-	}
-	public void setBookId(Long bookId) {
-		this.bookId = bookId;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public Double getPrice() {
-		return price;
-	}
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-	public int getStockQuantity() {
-		return stockQuantity;
-	}
-	public void setStockQuantity(int stockQuantity) {
-		this.stockQuantity = stockQuantity;
-	}
-	public AuthorDto getAuthor() {
-		return author;
-	}
-	public void setAuthor(AuthorDto author) {
-		this.author = author;
-	}
-	public CategoryDto getCategory() {
-		return category;
-	}
-	public void setCategory(CategoryDto category) {
-		this.category = category;
-	}
-	public BookDto(Long bookId, String title, Double price, int stockQuantity, AuthorDto author, CategoryDto category) {
-		super();
-		this.bookId = bookId;
-		this.title = title;
-		this.price = price;
-		this.stockQuantity = stockQuantity;
-		this.author = author;
-		this.category = category;
-	}
-	
-	
-	
-	
+
+	private String imageBase64;
+
 }
